41fc57b55d488aaadc2508013c24f3f1f8366f6d
Julieta <jarajuli13@gmail.com>
2025-01-28 00:33:51 -0300

    error apos

--- tauro/src/app/components/Hero.tsx
+++ tauro/src/app/components/Hero.tsx
@@ -2,7 +2,7 @@ export default function Hero() {
     return (
       <section className="bg-gray-200 text-gray-800 py-20">
         <div className="container mx-auto px-6 text-center">
-          <h1 className="text-5xl font-bold mb-4 text-gray-800">Bienvenidos a <span style={{ color: '#DC2626' }}>Tauro's</span></h1>
+          <h1 className="text-5xl font-bold mb-4 text-gray-800">Bienvenidos a <span style={{ color: '#DC2626' }}>Tauros</span></h1>
           <p className="text-xl mb-8 text-gray-600">
             El lugar perfecto para tus celebraciones, sean para niños o adultos
           </p>
